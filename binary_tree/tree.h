ec7855a940540209a993d2f92ac809de1dc83d8b
Ju Hong Kim <nabimoon1234@gmail.com>
2019-06-29 20:58:24 -0400

    Implemented array to BT

--- binary_tree/tree.h
+++ binary_tree/tree.h
@@ -2,6 +2,9 @@
 #define NULL 0
 #endif
 
+#define LEFT_CHILD(i) (i * 2 + 1)
+#define RIGHT_CHILD(i) ((i + 1) * 2)
+
 //Definition for a binary tree node.
 struct TreeNode {
     int val;
@@ -9,14 +12,22 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
-struct TreeNode* create_node(int val, int left, int right) {
-	struct TreeNode* node = malloc(sizeof(struct TreeNode));
-	if (!node) {
-		return(NULL);
+/*
+* Create binary tree
+*
+* param tree: an array of int
+* 	constraint: following rules apply:
+* 		parent node = i
+* 		left node   = i*2 + 1
+* 		right node  = (i+1) * 2
+*/
+struct TreeNode* create_node(int *tree_array, struct TreeNode *root, int i, int size) {
+	struct TreeNode * node = NULL;
+	if (i < size) {
+		node = malloc(sizeof(struct TreeNode));
+		node->val = tree_array[i];
+		node->left = create_node(tree_array, node, i*2 + 1, size);
+		node->right = create_node(tree_array, node, (i+1) * 2, size);
 	}
-	node->val = val;
-	node->left = left; 
-	node->right = right;
 	return(node);
 }
-
