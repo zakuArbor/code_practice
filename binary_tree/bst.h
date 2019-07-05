ea5baffd65a6bab1d31de2c3b97ea74c60aeae97
Ju Hong Kim <nabimoon1234@gmail.com>
2019-07-04 23:30:35 -0400

    Delete node in BST Implemented and Tested

--- binary_tree/bst.h
+++ binary_tree/bst.h
@@ -24,3 +24,49 @@ void insertNode(struct TreeNode **node, int i) {
 		(*node)->right = NULL;
 	}
 }
+
+struct TreeNode* find_min(struct TreeNode *node) {
+        struct TreeNode *curr = node;
+        while (curr) {
+                if (!curr->left) {
+                        break;
+                }
+                curr = curr->left;
+        }
+        return(curr);
+}
+
+struct TreeNode* delete_node(struct TreeNode *node, int i) {
+        if (!node) {
+                return(node);
+        }
+        if (node->val > i) {
+                node->left = delete_node(node->left, i);
+        }
+        else if (node->val < i) {
+                node->right = delete_node(node->right, i);
+        }
+        else {
+                struct TreeNode *tmp = NULL;
+
+                //one child only
+                if (!node->left) {
+                        tmp = node->right;
+                        free(node);
+                        node = NULL;
+                        return(tmp);
+                }
+                else if (!node->right) {
+                        tmp = node->left;
+                        free(node);
+                        node = NULL;
+                        return(tmp);
+                }
+                //2 children - get the successor (the min of the right subtree)
+                tmp = find_min(node->right);
+                node->val = tmp->val;
+                //delete the successor node since we replaced the node with the successor value
+		node->right = delete_node(node->right, node->val);
+        }
+	return(node);
+}
